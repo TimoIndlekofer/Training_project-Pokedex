0ca6627d62d5ad7fc24bb31dda020de89cdc267f
Timo Indlekofer <timo.indlekofer@web.de>
2026-06-09 14:40:05 +0200

    index.html:
    - update template for pokemon card
    - add init function to body
    
    standard.css:
    
    - update css class for main
    
    card-small.css:
    
    - update css class for pokemon card small
    - add css class for pokemon card small active
    
    script.js:
    
    - add variables for all pokemons and pokemon cards data
    - add functions for fetch Pokemon and fetch Pokemon details
    - add function for render Pokemons
    
    template.js:
    
    - add Pokemon template

---
+++ script.js
@@ -0,0 +1,49 @@
+// ##### JavaScript file for all functions #####
+
+let allPokemons = [];
+let pokemonCardsData = [];
+
+
+function init() {
+    console.log(allPokemons);
+    fetchPokemon();
+}
+
+
+async function fetchPokemon() {
+    try {
+        let response = await fetch('https://pokeapi.co/api/v2/pokemon?limit=20&offset=0');
+        let responseAsJSON = await response.json();
+        allPokemons = responseAsJSON.results;
+        renderPokemons();
+    } catch (error) {
+        console.log('Fehler beim Laden der Daten:', error);        
+    }
+}
+
+
+async function fetchPokemonDetails(pokemonURL) {
+    try {
+        const responseDetails = await fetch(pokemonURL);
+        const pokemonDetails = await responseDetails.json();
+        console.log(pokemonDetails);
+        pokemonCardsData += getPokemonTemplate(pokemonDetails);
+    } catch (error) {
+        console.log('Fehler beim Laden der Daten:', error);
+    }
+}
+
+
+async function renderPokemons() {
+    let pokemonCardsContainer = document.getElementById('pokemon-cards-container');
+    pokemonCardsContainer.innerHTML = '';
+
+    for (let i = 0; i < allPokemons.length; i++) {
+        const pokemonURL = allPokemons[i].url;
+        const pokemonDetails = await fetchPokemonDetails(pokemonURL);
+    }
+    pokemonCardsContainer.innerHTML = pokemonCardsData; 
+}
+
+
+
---
+++ scripts/template.js
@@ -0,0 +1,22 @@
+// ##### JavaScript file for templates #####
+
+function getPokemonTemplate(pokemonDetails) {
+    let pokemonTypes = `<span class="type-badge">${pokemonDetails.types[0].type.name}</span>`
+
+    if (pokemonDetails.types.length > 1) {
+        pokemonTypes += `<span class="type-badge">${pokemonDetails.types[1].type.name}</span>`
+    }
+
+    return `<button class="pokemon-card-small" data-id="card">
+                <span class="pokemon-card-small-id">#${pokemonDetails.id}</span>
+                <span class="pokemon-card-small-name">${pokemonDetails.name}</span>
+
+                <span class="pokemon-card-small-image-container">
+                    <img src="${pokemonDetails.sprites.other.home.front_default}" alt="Pokemon picture">
+                </span>
+
+                <span class="pokemon-card-small-badge-container">
+                    ${pokemonTypes}
+                </span>
+            </button>`
+}
