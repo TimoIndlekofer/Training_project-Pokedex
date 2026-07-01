5df2318b294c378f42a9c75e7f5b60e97af4042b
Timo Indlekofer <timo.indlekofer@web.de>
2026-07-01 16:24:53 +0200

    index.html:
    - remove html elements for dialog header and dialog main (about and base stats data)
    
    dialog.js:
    
    - remove variables for dialog tabs
    - add function getPokemonTypeClass
    - update functions addDataInDialogHeader, renderDialogHeader, fetchPokemonSpeciesDetails, addPokemonBasicData, addPokemonTrainingData, addPokemonEnvironmentData, renderDialogMainTabAbout, addPokemonBaseStatsData, renderDialogMainTabBaseStats
    - remove functions addBackgroundInDialogHeader, calculateBaseStatBars, addBackgroundToDialogStatBars and clearDataInDialogTabAbout
    
    template.js:
    
    - add functions getHeaderDataTemplate, getBasicDataTemplate, getTrainingDataTemplate, getEnvironmentDataTemplate and getBaseStatsDataTemplate

--- scripts/dialog.js
+++ scripts/dialog.js
@@ -5,12 +5,14 @@ const headerContainer = document.getElementById('header-container');
 const headerCloseButton = document.getElementById('header-close-button');
 const dialogTabButtons = document.querySelectorAll('.dialog-tab-button');
 const dialogTabContent = document.querySelectorAll('.dialog-tab-content');
-const aboutTab = document.getElementById('tab-about');
-const baseStatsTab = document.getElementById('tab-base-stats');
-const evolutionTab = document.getElementById('tab-evolution');
+const evolutionData = document.getElementById('data-evolution');
 const backButton = document.getElementById('dialog-back-button');
 const nextButton = document.getElementById('dialog-next-button');
-const evolutionData = document.getElementById('data-evolution');
+
+let aboutTabBasicData = document.getElementById('tab-about-basic-data');
+let aboutTabTrainingData = document.getElementById('tab-about-training-data');
+let aboutTabEnvironmentData = document.getElementById('tab-about-environment-data');
+let baseStatsData = document.getElementById('tab-base-stats-data');
 
 let currentPokemonID = 0;
 let allPokemonSpeciesDetails = [];
@@ -58,7 +60,6 @@ function closeDialog() {
 
     document.documentElement.classList.remove('dialog-no-scroll');
     dialogBox.close();
-    clearDataInDialogTabAbout();
 }
 
 
@@ -77,7 +78,6 @@ function closeDialogOnBackdropClick(event) {
     if (event.target === dialogBox) {
         dialogBox.close();
         document.documentElement.classList.remove('dialog-no-scroll');
-        clearDataInDialogTabAbout();
     }
 }
 
@@ -88,6 +88,14 @@ function closeDialogEscapeKey() {
             closeDialog();
         }
     });
+}
+
+
+function getPokemonTypeClass(singlePokemonID) {
+    const typeName = singlePokemonID.types[0].type.name;
+    const currentTypeClass = `bg-${typeName}`;
+
+    return currentTypeClass;
 }
 
 
@@ -104,16 +112,6 @@ async function renderPokemonInDialog(id) {
 }
 
 
-function addDataInDialogHeader(singlePokemonID) {
-    if (singlePokemonID) {
-        document.getElementById('header-id').textContent = "#" + singlePokemonID.id;
-        document.getElementById('header-name').textContent = singlePokemonID.name;
-        document.getElementById('header-img').src = singlePokemonID.sprites.other.home.front_default;
-        document.getElementById('header-badge1').textContent = singlePokemonID.types[0].type.name;
-    }
-}
-
-
 function checkBadgesInDialogHeader(singlePokemonID) {
     if (singlePokemonID.types.length === 1) {
         document.getElementById('header-badge2').textContent = '';
@@ -125,18 +123,19 @@ function checkBadgesInDialogHeader(singlePokemonID) {
 }
 
 
-function addBackgroundInDialogHeader(singlePokemonID) {
-    const allTypes = ['normal', 'fire', 'water', 'electric', 'grass', 'ice', 'fighting', 'poison', 'ground', 'flying', 'psychic', 'bug', 'rock', 'ghost', 'dragon', 'dark', 'steel', 'fairy'];
-    const typeName = singlePokemonID.types[0].type.name;
+function addDataInDialogHeader(singlePokemonID) {
+    headerContainer.innerHTML = '';
 
-    for (let index = 0; index < allTypes.length; index++) {
-        const className = 'bg-' + allTypes[index];
+    const pokemonID = "#" + singlePokemonID.id;
+    const pokemonName = singlePokemonID.name
+    const pokemonIMG = singlePokemonID.sprites.other.home.front_default;
+    const pokemonBadge = singlePokemonID.types[0].type.name;
 
-        headerContainer.classList.remove(className);
-        headerCloseButton.classList.remove(className);        
-    }
-    headerContainer.classList.add('bg-' + typeName);
-    headerCloseButton.classList.add('bg-' + typeName);
+    const currentTypeClass = getPokemonTypeClass(singlePokemonID);
+
+    headerContainer.className = 'dialog-header-container ' + currentTypeClass;
+    
+    headerContainer.innerHTML = getHeaderDataTemplate(pokemonID, pokemonName, pokemonIMG, pokemonBadge, currentTypeClass);
 }
 
 
@@ -145,11 +144,13 @@ function renderDialogHeader(id) {
 
     addDataInDialogHeader(singlePokemonID);
     checkBadgesInDialogHeader(singlePokemonID);
-    addBackgroundInDialogHeader(singlePokemonID);
 }
 
 
 async function fetchPokemonSpeciesDetails(singlePokemonID) {
+    const pokemonSpeciesDetailsAvailable = allPokemonSpeciesDetails.find(pokemon => pokemon.id === singlePokemonID.id);
+    if (pokemonSpeciesDetailsAvailable) return pokemonSpeciesDetailsAvailable;
+
     try {
         const responseDetails = await fetch(singlePokemonID.species.url);
         const pokemonSpeciesDetails = await responseDetails.json();
@@ -184,61 +185,50 @@ function getPokemonEggGroups(trainingData) {
 }
 
 
-async function addPokemonBasicData(singlePokemonID) {
-    const speciesElement = aboutTab.querySelector('[data-pokemon="species"]');
-    speciesElement.textContent = '';
+function addPokemonBasicData(speciesData, singlePokemonID) {
+    aboutTabBasicData.innerHTML = '';
     
-    const speciesData = await fetchPokemonSpeciesDetails(singlePokemonID);
     const speciesDataEntry = speciesData.genera.find(data => data.language.name === 'en');
 
-    aboutTab.querySelector('[data-pokemon="species"]').textContent = speciesDataEntry.genus;
-    aboutTab.querySelector('[data-pokemon="height"]').textContent = (singlePokemonID.height / 10).toFixed(1).replace('.', ',') + " m";
-    aboutTab.querySelector('[data-pokemon="weight"]').textContent = (singlePokemonID.weight / 10).toFixed(1).replace('.', ',') + " kg";
+    const pokemonGenus = speciesDataEntry ? speciesDataEntry.genus : "Unknown";
+    const pokemonHeight = (singlePokemonID.height / 10).toFixed(1).replace('.', ',') + " m";
+    const pokemonWeight = (singlePokemonID.weight / 10).toFixed(1).replace('.', ',') + " kg";
     
-    const ability = getPokemonAbilities(singlePokemonID.abilities);
-    aboutTab.querySelector('[data-pokemon="abilities"]').textContent = ability;
+    const pokemonAbilities = getPokemonAbilities(singlePokemonID.abilities);
+
+    aboutTabBasicData.innerHTML = getBasicDataTemplate(pokemonGenus, pokemonHeight, pokemonWeight, pokemonAbilities);
 }
 
 
-function addPokemonTrainingData(trainingData, singlePokemonID) {
-    aboutTab.querySelector('[data-pokemon="catch-rate"]').textContent = trainingData.capture_rate;
-    aboutTab.querySelector('[data-pokemon="base-exp"]').textContent = singlePokemonID.base_experience + " EP";
-    aboutTab.querySelector('[data-pokemon="growth"]').textContent = trainingData.growth_rate.name.charAt(0).toUpperCase() + trainingData.growth_rate.name.slice(1);
+function addPokemonTrainingData(speciesData, singlePokemonID) {
+    aboutTabTrainingData.innerHTML = '';
+
+    const pokemonCaptureRate = speciesData.capture_rate;
+    const pokemonBaseExperience = singlePokemonID.base_experience + " EP";
+    const pokemonGrowth = speciesData.growth_rate && speciesData.growth_rate.name ? speciesData.growth_rate.name.charAt(0).toUpperCase() + speciesData.growth_rate.name.slice(1) : "Unknown";
+
+    aboutTabTrainingData.innerHTML = getTrainingDataTemplate(pokemonCaptureRate, pokemonBaseExperience, pokemonGrowth);
 }
 
 
-function addPokemonEnvironmentData(trainingData) {
-    const eggGroup = getPokemonEggGroups(trainingData);
-    aboutTab.querySelector('[data-pokemon="egg-groups"]').textContent = eggGroup;
+function addPokemonEnvironmentData(speciesData) {
+    aboutTabEnvironmentData.innerHTML = '';
 
-    if (trainingData.habitat) {
-        const habitatName = trainingData.habitat.name;
-        aboutTab.querySelector('[data-pokemon="habitat"]').textContent = habitatName.charAt(0).toUpperCase() + habitatName.slice(1);
-    } else {
-        aboutTab.querySelector('[data-pokemon="habitat"]').textContent = "Unknown";
-    }
+    const eggGroup = getPokemonEggGroups(speciesData);
+    const habitatName = speciesData.habitat && speciesData.habitat.name ? speciesData.habitat.name.charAt(0).toUpperCase() + speciesData.habitat.name.slice(1) : "Unknown";
+
+    aboutTabEnvironmentData.innerHTML = getEnvironmentDataTemplate(eggGroup, habitatName);
 }
 
 
 async function renderDialogMainTabAbout(id) {
     const singlePokemonID = allPokemonDetails.find(pokemon => pokemon.id === id);
 
-    await addPokemonBasicData(singlePokemonID);
+    const speciesData = await fetchPokemonSpeciesDetails(singlePokemonID);
 
-    const trainingData = allPokemonSpeciesDetails.find(pokemon => pokemon.id === id);
-    
-    addPokemonTrainingData(trainingData, singlePokemonID);
-    addPokemonEnvironmentData(trainingData);
-}
-
-
-function addPokemonBaseStatData(singlePokemonID) {
-    baseStatsTab.querySelector('[data-pokemon="hp"]').textContent = singlePokemonID.stats[0].base_stat;
-    baseStatsTab.querySelector('[data-pokemon="attack"]').textContent = singlePokemonID.stats[1].base_stat;
-    baseStatsTab.querySelector('[data-pokemon="defense"]').textContent = singlePokemonID.stats[2].base_stat;
-    baseStatsTab.querySelector('[data-pokemon="special-attack"]').textContent = singlePokemonID.stats[3].base_stat;
-    baseStatsTab.querySelector('[data-pokemon="special-defense"]').textContent = singlePokemonID.stats[4].base_stat;
-    baseStatsTab.querySelector('[data-pokemon="speed"]').textContent = singlePokemonID.stats[5].base_stat;
+    addPokemonBasicData(speciesData, singlePokemonID);
+    addPokemonTrainingData(speciesData, singlePokemonID);
+    addPokemonEnvironmentData(speciesData);
 }
 
 
@@ -250,45 +240,31 @@ function calculateBaseStatTotalValue(singlePokemonID) {
 
         totalValue += value;
     }
-    baseStatsTab.querySelector('[data-pokemon="total"]').textContent = totalValue;
+    return totalValue;
 }
 
 
-function calculateBaseStatBars(singlePokemonID) {
-    const maxValue = 255;
+function addPokemonBaseStatsData(singlePokemonID) {
+    baseStatsData.innerHTML = '';
 
-    baseStatsTab.querySelector('[data-pokemon="hp-bar"]').style.width = ((singlePokemonID.stats[0].base_stat / maxValue) * 100) + '%';
-    baseStatsTab.querySelector('[data-pokemon="attack-bar"]').style.width = ((singlePokemonID.stats[1].base_stat / maxValue) * 100) + '%';
-    baseStatsTab.querySelector('[data-pokemon="defense-bar"]').style.width = ((singlePokemonID.stats[2].base_stat / maxValue) * 100) + '%';
-    baseStatsTab.querySelector('[data-pokemon="special-attack-bar"]').style.width = ((singlePokemonID.stats[3].base_stat / maxValue) * 100) + '%';
-    baseStatsTab.querySelector('[data-pokemon="special-defense-bar"]').style.width = ((singlePokemonID.stats[4].base_stat / maxValue) * 100) + '%';
-    baseStatsTab.querySelector('[data-pokemon="speed-bar"]').style.width = ((singlePokemonID.stats[5].base_stat / maxValue) * 100) + '%';
-}
+    const currentTypeClass = getPokemonTypeClass(singlePokemonID);
+  
+    const baseStatHP = singlePokemonID.stats[0].base_stat;
+    const baseStatAttack = singlePokemonID.stats[1].base_stat;
+    const baseStatDefense = singlePokemonID.stats[2].base_stat;
+    const baseStatSpecialAttack = singlePokemonID.stats[3].base_stat;
+    const baseStatSpecialDefense = singlePokemonID.stats[4].base_stat;
+    const baseStatSpeed = singlePokemonID.stats[5].base_stat;
+    const totalBaseStatsValue = calculateBaseStatTotalValue(singlePokemonID);
 
-
-function addBackgroundToDialogStatBars(singlePokemonID) {
-    const typeName = singlePokemonID.types[0].type.name;
-    const currentTypeClass = `bg-${typeName}`;
-
-    singlePokemonID.stats.forEach(data => {
-        const statName = data.stat.name;
-        const statBar = document.querySelector(`.dialog-stat-bar[data-pokemon="${statName}-bar"]`)
-
-        if(statBar) {
-            statBar.className = 'dialog-stat-bar';
-            statBar.classList.add(currentTypeClass);
-        }
-    });
+    baseStatsData.innerHTML = getBaseStatsDataTemplate(baseStatHP, baseStatAttack, baseStatDefense, baseStatSpecialAttack, baseStatSpecialDefense, baseStatSpeed, totalBaseStatsValue, currentTypeClass);
 }
 
 
 async function renderDialogMainTabBaseStats(id) {
     const singlePokemonID = allPokemonDetails.find(pokemon => pokemon.id === id);
 
-    addPokemonBaseStatData(singlePokemonID);
-    calculateBaseStatTotalValue(singlePokemonID);
-    calculateBaseStatBars(singlePokemonID);
-    addBackgroundToDialogStatBars(singlePokemonID);
+    addPokemonBaseStatsData(singlePokemonID);
 }
 
 
@@ -299,7 +275,6 @@ async function fetchPokemonEvolutionChain(id) {
     try {
         const responseDetails = await fetch(evolutionChainURL);
         const pokemonEvolutionDetails = await responseDetails.json();
-
         pokemonEvolutionChain.push(pokemonEvolutionDetails);
     } catch (error) {
         console.log('Fehler beim Laden der Daten:', error);
@@ -404,7 +379,6 @@ function checkNextButtonInDialog() {
 
 function backButtonInDialog() {
     const singlePokemonID = currentViewOfPokemons.findIndex(pokemon => Number(pokemon.id) === Number(currentPokemonID));
-    clearDataInDialogTabAbout();
 
     if (singlePokemonID > 0) {
         const previousPokemonID = currentViewOfPokemons[singlePokemonID - 1];
@@ -415,20 +389,10 @@ function backButtonInDialog() {
 
 function nextButtonInDialog() {
     const singlePokemonID = currentViewOfPokemons.findIndex(pokemon => Number(pokemon.id) === Number(currentPokemonID));
-    clearDataInDialogTabAbout();
 
     if (singlePokemonID !== -1 && singlePokemonID < currentViewOfPokemons.length - 1) {
         const nextPokemonID = currentViewOfPokemons[singlePokemonID + 1];
         renderPokemonInDialog(nextPokemonID.id);
-    }
-}
-
-
-function clearDataInDialogTabAbout() {
-    const aboutTabData = document.querySelectorAll('[data-pokemon]');
-
-    for (let index = 0; index < aboutTabData.length; index++) {
-        aboutTabData[index].innerText = '';;        
     }
 }
 
@@ -440,3 +404,4 @@ function stopPropagationInDialog() {
         event.stopPropagation();
     });
 }
+
--- scripts/template.js
+++ scripts/template.js
@@ -1,5 +1,7 @@
 // ##### JavaScript file for templates #####
 
+// Pokemon cards on main page: 
+
 function getPokemonTemplate(pokemonDetails) {
     let pokemonTypes = `<span class="type-badge">${pokemonDetails.types[0].type.name}</span>`
 
@@ -19,6 +21,111 @@ function getPokemonTemplate(pokemonDetails) {
                     ${pokemonTypes}
                 </span>
             </button>`
+}
+
+
+// Dialog:
+
+function getHeaderDataTemplate(pokemonID, pokemonName, pokemonIMG, pokemonBadge, currentTypeClass) {
+    return `<div class="dialog-header-top">
+                    <span class="dialog-header-id" id="header-id">${pokemonID}</span>
+                    <button class="dialog-header-close-button ${currentTypeClass}" id="header-close-button" data-id="close-dialog-button" onclick="closeDialog()">X</button>
+                </div>
+                <div class="dialog-header-bottom">
+                    <h3 id="header-name">${pokemonName}</h3>
+                    <div class="dialog-header-bottom-badges-container">
+                        <span class="type-badge" id="header-badge1">${pokemonBadge}</span>
+                        <span class="type-badge" id="header-badge2"></span>
+                    </div>
+                    <img src="${pokemonIMG}" alt="Pokemon picture" class="dialog-img" id="header-img" data-id="dialog-image">
+            </div>`
+}
+
+
+function getBasicDataTemplate(pokemonGenus, pokemonHeight, pokemonWeight, pokemonAbilities) {
+    return `<dt>Species</dt>
+                        <dd>${pokemonGenus}</dd>
+
+                        <dt>Height</dt>
+                        <dd>${pokemonHeight}</dd>
+
+                        <dt>Weight</dt>
+                        <dd>${pokemonWeight}</dd>
+
+                        <dt>Abilities</dt>
+                        <dd>${pokemonAbilities}</dd>`
+}
+
+
+function getTrainingDataTemplate(pokemonCaptureRate, pokemonBaseExperience, pokemonGrowth) {
+    return `<dt>Catch rate</dt>
+                        <dd>${pokemonCaptureRate}</dd>
+
+                        <dt>Base exp.</dt>
+                        <dd>${pokemonBaseExperience}</dd>
+
+                        <dt>Growth</dt>
+                        <dd>${pokemonGrowth}</dd>`
+}
+
+
+function getEnvironmentDataTemplate(eggGroup, habitatName) {
+    return `<dt>Egg groups</dt>
+                        <dd>${eggGroup}</dd>
+
+                        <dt>Habitat</dt>
+                        <dd>${habitatName}</dd>`
+}
+
+
+function getBaseStatsDataTemplate(baseStatHP, baseStatAttack, baseStatDefense, baseStatSpecialAttack, baseStatSpecialDefense, baseStatSpeed, totalBaseStatsValue, currentTypeClass) {
+    return `<dt>HP</dt>
+                        <dd>
+                            <span class="dialog-stat-value" data-pokemon="hp">${baseStatHP}</span>
+                            <div class="dialog-stat-bar-container">
+                                <div class="dialog-stat-bar ${currentTypeClass}" style="width: ${baseStatHP + '%'}"></div>
+                            </div>
+                        </dd>
+                        <dt>Attack</dt>
+                        <dd>
+                            <span class="dialog-stat-value" data-pokemon="attack">${baseStatAttack}</span>
+                            <div class="dialog-stat-bar-container">
+                                <div class="dialog-stat-bar ${currentTypeClass}" style="width: ${baseStatAttack + '%'}"></div>
+                            </div>
+                        </dd>
+                        <dt>Defense</dt>
+                        <dd>
+                            <span class="dialog-stat-value" data-pokemon="defense">${baseStatDefense}</span>
+                            <div class="dialog-stat-bar-container">
+                                <div class="dialog-stat-bar ${currentTypeClass}" style="width: ${baseStatDefense + '%'}"></div>
+                            </div>
+                        </dd>
+                        <dt>Sp. Attack</dt>
+                        <dd>
+                            <span class="dialog-stat-value" data-pokemon="special-attack">${baseStatSpecialAttack}</span>
+                            <div class="dialog-stat-bar-container">
+                                <div class="dialog-stat-bar ${currentTypeClass}" style="width: ${baseStatSpecialAttack + '%'}"></div>
+                            </div>
+                        </dd>
+                        <dt>Sp. Defense</dt>
+                        <dd>
+                            <span class="dialog-stat-value" data-pokemon="special-defense">${baseStatSpecialDefense}</span>
+                            <div class="dialog-stat-bar-container">
+                                <div class="dialog-stat-bar ${currentTypeClass}" style="width: ${baseStatSpecialDefense + '%'}"></div>
+                            </div>
+                        </dd>
+                        <dt>Speed</dt>
+                        <dd>
+                            <span class="dialog-stat-value" data-pokemon="speed">${baseStatSpeed}</span>
+                            <div class="dialog-stat-bar-container">
+                                <div class="dialog-stat-bar ${currentTypeClass}" style="width: ${baseStatSpeed + '%'}"></div>
+                            </div>
+                        </dd>
+                        <div class="dialog-stat-line"></div>
+                        <dt class="dialog-stat-total-label">Total</dt>
+                        <dd>
+                            <span class="dialog-stat-total-value">${totalBaseStatsValue}</span>
+                        </dd>`
 }
 
 
